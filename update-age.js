0f682f6fdaef280df32bee1833df0bdd6ca1a10f
isRael Almeida <almeidev.rael@gmail.com>
2025-07-29 19:39:38 -0300

    Create update-age.js
    Calculate current age

---
+++ update-age.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const path = require('path')
+
+const BIRTH_DATE = new Date (1998, 10, 29)
+
+function calculateAge(birthDate) {
+  const today = new Date()
+  let age = today.getFullYear() - birthDate.getFullYear()
+  const m = today.getMonth() - birthDate.getMonth()
+
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--
+  }
+  return age
+}
+
+const currentAge - calculateAge(BIRTH_DATE)
+
+const readmePath = path.join(__dirname, 'README.md')
+let readmeContent = fs.readFileSync(readmePath, 'utf8')
+
+const ageRegex = /(\d+)/
+
+if (readmeContent.match(ageRegex)) {
+  readmeContent = readmeContent.replace(ageRegex, `${currentAge}`)
+  fs.writeFileSync(readmePath, readmeContent)
+  console.log(`Idade atualizada para: ${currentAge} no README.md`)
+} else {
+  console.error('Marcadores e não encontrados no README.md. Por favor, adicione-os.')
+}
