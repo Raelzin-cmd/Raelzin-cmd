8dd22c635e1626965dc5680e25e4a068e058d8db
Israel Almeida <almeidev.rael@gmail.com>
2025-12-02 23:15:39 -0300

    Fix Placeholder

--- update-age.js
+++ update-age.js
@@ -1,30 +1,34 @@
 const fs = require('fs')
 const path = require('path')
 
-const BIRTH_DATE = new Date (1998, 10, 29)
+const BIRTH_DATE = new Date(1998, 10, 29)
 
-function calculateAge(birthDate) {
+function calcAge(birthDate) {
   const today = new Date()
   let age = today.getFullYear() - birthDate.getFullYear()
   const m = today.getMonth() - birthDate.getMonth()
 
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-    age--
+  if (m < 0 || (m == 0 && today.getDate() < birthDate.getDate())) {
+    age --
   }
   return age
 }
 
-const currentAge = calculateAge(BIRTH_DATE)
+
+const currentAge = calcAge(BIRTH_DATE)
+
 
 const readmePath = path.join(__dirname, 'README.md')
 let readmeContent = fs.readFileSync(readmePath, 'utf8')
 
-const ageRegex = /{{AGE}}/
 
-if (readmeContent.match(ageRegex)) {
-  readmeContent = readmeContent.replace(ageRegex, `${currentAge}`);
-  fs.writeFileSync(readmePath, readmeContent);
-  console.log(`README updated with age: ${currentAge}`);
+const ageRegex = /\{\{AGE\}\}/g
+
+
+if (ageRegex.test(readmeContent)) {
+  readmeContent = readmeContent.replace(ageRegex, String(currentAge))
+  fs.writeFileSync(readmePath, readmeContent)
+  console.log(`README atualizado com a idade: ${currentAge}`)
 } else {
-  console.log("Placeholder '(update-age.js here)' not found in README. No update performed.");
+  console.log("Placeholder '{{AGE}}' não encontrado no README. Nenhuma atualização realizada")
 }
